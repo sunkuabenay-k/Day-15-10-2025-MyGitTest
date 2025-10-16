fecd7347bc278d979e7920dd6e77271278882958
sunkuabenay-k <sunkuabenay.k@encora.com>
2025-10-16 12:08:35 +0530

    Add files via upload

---
+++ MyGitTest/src/main/java/com/example/MyGitTest/App.java
@@ -0,0 +1,11 @@
+package com.example.MyGitTest;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello from MyApp!");
+    }
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
